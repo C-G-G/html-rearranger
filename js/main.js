c44619df06a5be75d0504adb15d054e6961bb08c
CGG <16348315+C-G-G@users.noreply.github.com>
2021-11-08 21:07:46 -0500

    Add comments on top of the code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// Create a heading: “Below ground veggies”
+// Create a list to hold the below ground veggies
+// Add the heading and list to the document
+// Create a second heading for “Above ground veggies” and add it to the document
+// Create a list to hold the above ground veggies and add it to the document
+
 var $h2Below = $('<h2>Below ground veggies</h2>');
 var $ulBelow = $('<ul>');
 var $h2Above = $('<h2>Above ground veggies</h2>');
